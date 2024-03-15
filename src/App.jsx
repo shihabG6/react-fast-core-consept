11e32514d80c78ac5df5f187f70bfe9c86e691f7
shihabG6 <shihabuddin0194@gmail.com>
2024-03-15 16:28:06 +0600

    repit

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,15 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <h1>React Core Concepts</h1>
+      <ol>
+        <li>Components</li>
+        <li>JSX</li>
+        <li>Props</li>
+        <li>Event Handler</li>
+        <li>State</li>
+        <li>Load data</li>
+      </ol>
       <Posts></Posts>
     </>
   )
